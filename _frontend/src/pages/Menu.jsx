5efc7242a3c9855116e15dd6dd792b81c7d6d37f
MeryemCodes <bouzirmeryemm@gmail.com>
2025-03-27 16:49:40 +0100

    filters

--- _frontend/src/pages/Menu.jsx
+++ _frontend/src/pages/Menu.jsx
@@ -1,12 +1,40 @@
-import FilterBar from "../components/layout/FilterBar"
+import { useState } from "react";
+import FilterBar from "../components/layout/FilterBar";
 import PlateContainer from "../components/layout/PlateContainer";
+import { FaFilter, FaTimes } from "react-icons/fa";
 
- const Menu = () => {
+const Menu = () => {
+  const [showFilters, setShowFilters] = useState(false);
+
   return (
-    <div className="grid grid-cols-4 gap-4">
-      <FilterBar />
-      <PlateContainer />
+    <div className="relative">
+      <button
+        className="fixed top-4 left-4 bg-prime p-2 rounded-full text-white z-50"
+        onClick={() => setShowFilters(!showFilters)}
+      >
+        {showFilters ? <FaTimes className="w-6 h-6" /> : <FaFilter className="w-6 h-6" />}
+      </button>
+
+      {showFilters && (
+        <div
+          className="fixed inset-0 bg-black/40 z-40"
+          onClick={() => setShowFilters(false)}
+        ></div>
+      )}
+
+      <div
+        className={`fixed bottom-4 left-0 h-[calc(100vh-5rem)] w-1/2 bg-dark transform ${
+          showFilters ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 z-50 overflow-y-auto`}
+      >
+        <FilterBar />
+      </div>
+
+      <div>
+        <PlateContainer />
+      </div>
     </div>
   );
-}
-export default Menu
+};
+
+export default Menu;
